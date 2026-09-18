bf24a9f969c867f7b8fa680a8d2f01c9ac245fcb
babakbit2022-maker <babakbit2022@gmail.com>
2026-09-18 12:17:07 +0330

    Add PM2 production configuration

---
+++ ecosystem.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [{
+    name: 'cryptopilot',
+    script: 'bootstrap.js',
+    cwd: '/opt/cryptopilot',
+    instances: 1,
+    exec_mode: 'fork',
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '700M',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 3000
+    }
+  }]
+};
